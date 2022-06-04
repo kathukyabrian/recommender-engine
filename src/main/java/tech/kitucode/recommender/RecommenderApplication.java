00c003e8ea4f9c6a3f8d2350d12070e9e8d37c40
kathukyabrian <briankathukya2000@gmail.com>
2022-06-04 09:35:45 +0300

    Added AOP for logging purposes

--- src/main/java/tech/kitucode/recommender/RecommenderApplication.java
+++ src/main/java/tech/kitucode/recommender/RecommenderApplication.java
@@ -3,8 +3,10 @@ package tech.kitucode.recommender;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import tech.kitucode.recommender.config.ApplicationProperties;
 
+@EnableAspectJAutoProxy(proxyTargetClass = true)
 @SpringBootApplication
 @EnableConfigurationProperties(ApplicationProperties.class)
 public class RecommenderApplication {
---
+++ src/main/java/tech/kitucode/recommender/aop/EventServiceAspect.java
@@ -0,0 +1,22 @@
+package tech.kitucode.recommender.aop;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+public class EventServiceAspect {
+
+    private final Logger logger = LoggerFactory.getLogger(EventServiceAspect.class);
+
+    @Before(value = "execution(* tech.kitucode.recommender.service.EventService.*(..))")
+    public void beforeAdvice(JoinPoint joinPoint){
+        logger.info("Before method : {}", joinPoint.getSignature());
+        logger.info("");
+    }
+
+}
